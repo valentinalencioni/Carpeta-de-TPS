f470ead647bff320a0883456bd3cd91149e5bb37
valentinalencioni <143528811+valentinalencioni@users.noreply.github.com>
2023-09-26 13:37:22 -0300

    Add files via upload

---
+++ TypeScript/index.js
@@ -0,0 +1,21 @@
+;
+fetch('https://fakestoreapi.com/products')
+    .then(function (res) { return res.json(); })
+    .then(function (products) {
+    console.log('products', products);
+    // Prepare table HTML
+    var tableHTML = '<thead><tr><th>ID</th><th>Title</th><th>Description</th><th>Price</th> </tr></thead><tbody>';
+    // Loop thru all products to generate rows of the table
+    products.forEach(function (p) {
+        tableHTML += "<tr><td>".concat(p.id, "</td><td>").concat(p.title, "</td><td>").concat(p.description, "</td><td>").concat(p.price, "</td></tr>");
+    });
+    // // Close table body
+    tableHTML += '</tbody>';
+    // Grab table element to set its inner HTML
+    document.querySelector('#tableElement').innerHTML = tableHTML;
+    // const tableElement: HTMLElement=document.querySelector('#tableElement');
+    //     tableElement.innerHTML=tableHTML;
+    // Hide spinner 
+    var spinnerElement = document.querySelector('#spinnerContainer');
+    spinnerElement.style.display = 'none';
+});
